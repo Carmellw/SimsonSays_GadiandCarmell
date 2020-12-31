df4708399ea868f12a473a30300c38f89153f19a
Carmellw <carmellwass@gmail.com>
2020-12-31 17:17:26 +0200

    a

--- SimsonSays_GadiandCarmell/Form1.cs
+++ SimsonSays_GadiandCarmell/Form1.cs
@@ -10,11 +10,45 @@ using System.Windows.Forms;
 
 namespace SimsonSays_GadiandCarmell
 {
+    
     public partial class Form1 : Form
     {
+        private PictureBox[] PictureBoxArr = new PictureBox[4];
+        Random rnd = new Random();
+        private int[] order = new int[4];
+       
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void SetArr()
+        {
+            PictureBoxArr[0] = pictureBox1;
+            PictureBoxArr[1] = pictureBox2;
+            PictureBoxArr[2] = pictureBox3;
+            PictureBoxArr[3] = pictureBox4;
+
+            for (int i = 0; i < order.Length; i++)
+            {
+                order[i] = rnd.Next(0, 4);
+            }
+        }
+
+       
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
         }
     }
 }
